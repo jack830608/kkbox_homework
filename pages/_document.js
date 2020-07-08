6c56934c5bfbd36a9d1c27367e65045822ba223b
JackLin <jack830608@gmail.com>
2020-07-08 17:27:34 +0800

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,7 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0" />
                     <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
+                    <link rel="icon" type="image/png" href="/images/favicon.png"></link>
                     <title>KKBOX</title>
                 </Head>
                 <body style={{ margin: 0, fontFamily: "'Noto Sans TC','Hind Siliguri', sans-serif" }}>
